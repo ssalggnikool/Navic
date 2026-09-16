1690076fea5e00a7bc1a638f2577db1ada6043f4
Nightdavisao <nightdavisao27@protonmail.com>
2026-09-16 20:42:52 -0300

    fix: i love gradle

--- composeApp/src/androidMain/kotlin/paige/navic/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/paige/navic/di/PlatformModule.android.kt
@@ -55,7 +55,6 @@ actual val platformModule = module {
 			downloadManager = get(),
 			connectivityManager = get(),
 			sessionManager = get(),
-			platformContext = get(),
 			preferenceManager = get(),
 			snackBarManager = get(),
 			audioGainManager = get()
