dab1f06667aa66f18d379880cad054e22af909cb
Nightdavisao <nightdavisao27@protonmail.com>
2026-09-16 19:53:56 -0300

    wip(oom-fix): don't save every image to memory on `DomainSong.toMediaItem()`

--- composeApp/src/androidMain/kotlin/paige/navic/shared/MediaPlayer.android.kt
+++ composeApp/src/androidMain/kotlin/paige/navic/shared/MediaPlayer.android.kt
@@ -462,7 +462,6 @@ class AndroidMediaPlayerViewModel(
 	private val audioGainManager: AudioGainManager,
 	private val application: Application,
 	private val albumDao: AlbumDao,
-	private val platformContext: CoilPlatformContext,
 	private val sessionManager: SessionManager,
 	private val snackBarManager: SnackBarManager
 ) : MediaPlayerViewModel(
@@ -1151,30 +1150,6 @@ class AndroidMediaPlayerViewModel(
 			.setAlbumTitle(albumTitle)
 			.setDurationMs(duration.inWholeMilliseconds)
 			.setMediaType(MediaMetadata.MEDIA_TYPE_MUSIC)
-
-		val artworkData = coverArtId?.let { coverId ->
-			val diskCache = platformContext.imageLoader.diskCache
-			val snapshot = diskCache?.openSnapshot(coverId) ?: return@let null
-
-			val bytes = try {
-				snapshot.use { it.data.toFile().readBytes() }
-			} catch (ex: Exception) {
-				Logger.w("MediaPlayer", "could not read artwork data", ex)
-				null
-			}
-
-			snapshot.close()
-
-			return@let bytes
-		}
-
-		if (artworkData != null) {
-			metadataBuilder.setArtworkData(artworkData, MediaMetadata.PICTURE_TYPE_FRONT_COVER)
-		} else {
-			metadataBuilder.setArtworkUri(
-				coverArtId?.let { sessionManager.getCoverArtUrl(it).toUri() }
-			)
-		}
 
 		val metadata = metadataBuilder.build()
 
