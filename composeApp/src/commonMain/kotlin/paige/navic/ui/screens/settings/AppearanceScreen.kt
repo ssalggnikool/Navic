f4be3e696354460fdb512ab4c955bff6e83d7de6
Nightdavisao <nightdavisao27@protonmail.com>
2026-09-16 16:44:35 -0300

    fix: leftover from merge

--- composeApp/src/commonMain/kotlin/paige/navic/ui/screens/settings/AppearanceScreen.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/screens/settings/AppearanceScreen.kt
@@ -216,33 +216,26 @@ fun SettingsAppearanceScreen() {
 						supportingContent = { Text(stringResource(Res.string.subtitle_dynamic_theming)) },
 						checked = preferenceManager.dynamicTheming,
 						onCheckedChange = { preferenceManager.dynamicTheming = it },
-						shapes = SegmentedListItemDefaults.segmentedShapes(index = 0, count = 7)
+						shapes = SegmentedListItemDefaults.segmentedShapes(index = 0, count = 6)
 					)
 					SettingsToggleItem(
 						content = { Text(stringResource(Res.string.option_alphabetical_scroll)) },
 						checked = preferenceManager.alphabeticalScroll,
 						onCheckedChange = { preferenceManager.alphabeticalScroll = it },
-						shapes = SegmentedListItemDefaults.segmentedShapes(index = 1, count = 7)
+						shapes = SegmentedListItemDefaults.segmentedShapes(index = 1, count = 6)
 					)
 					SettingsToggleItem(
 						content = { Text(stringResource(Res.string.option_enable_predictive_back_animations)) },
 						checked = preferenceManager.enablePredictiveBackAnimations,
 						onCheckedChange = { preferenceManager.enablePredictiveBackAnimations = it },
-						shapes = SegmentedListItemDefaults.segmentedShapes(index = 2, count = 7)
+						shapes = SegmentedListItemDefaults.segmentedShapes(index = 2, count = 6)
 					)
 					SettingsToggleItem(
 						content = { Text(stringResource(Res.string.option_enable_ratings)) },
 						supportingContent = { Text(stringResource(Res.string.subtitle_enable_ratings)) },
 						checked = preferenceManager.enableRatings,
 						onCheckedChange = { preferenceManager.enableRatings = it },
-						shapes = SegmentedListItemDefaults.segmentedShapes(index = 3, count = 7)
-					)
-					SettingsToggleItem(
-						content = { Text(stringResource(Res.string.option_enable_sharing)) },
-						supportingContent = { Text(stringResource(Res.string.subtitle_enable_sharing)) },
-						checked = preferenceManager.enableSharing,
-						onCheckedChange = { preferenceManager.enableSharing = it },
-						shapes = SegmentedListItemDefaults.segmentedShapes(index = 4, count = 7)
+						shapes = SegmentedListItemDefaults.segmentedShapes(index = 3, count = 6)
 					)
 					SettingsChoiceItem(
 						content = { Text(stringResource(Res.string.option_use_marquee_text)) },
@@ -250,7 +243,7 @@ fun SettingsAppearanceScreen() {
 						selectedChoice = preferenceManager.marqueeSpeed,
 						onChoiceSelected = { preferenceManager.marqueeSpeed = it },
 						label = { it.name },
-						shapes = SegmentedListItemDefaults.segmentedShapes(index = 5, count = 7)
+						shapes = SegmentedListItemDefaults.segmentedShapes(index = 4, count = 6)
 					)
 					SettingsChoiceItem(
 						content = { Text(stringResource(Res.string.option_animation_style)) },
@@ -259,7 +252,7 @@ fun SettingsAppearanceScreen() {
 						onChoiceSelected = { preferenceManager.animationStyle = it },
 						label = { it.name },
 						// 6767676767
-						shapes = SegmentedListItemDefaults.segmentedShapes(index = 6, count = 7)
+						shapes = SegmentedListItemDefaults.segmentedShapes(index = 5, count = 6)
 					)
 				}
 			}
