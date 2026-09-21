de4ec9ba851cc9375d73488152dadcccc8335268
Nightdavisao <nightdavisao27@protonmail.com>
2026-09-21 11:50:35 -0300

    i love having headaches

--- composeApp/src/commonMain/kotlin/paige/navic/ui/screens/settings/ConnectionOptionsScreen.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/screens/settings/ConnectionOptionsScreen.kt
@@ -1,12 +1,12 @@
 package paige.navic.ui.screens.settings
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -14,32 +14,28 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.ToggleButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.runtime.snapshots.SnapshotStateSet
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import navic.composeapp.generated.resources.Res
@@ -48,14 +44,15 @@ import navic.composeapp.generated.resources.action_new
 import navic.composeapp.generated.resources.info_incorrect_proxy_url_format
 import navic.composeapp.generated.resources.info_proxy_url_placeholder
 import navic.composeapp.generated.resources.option_connection_options
-import navic.composeapp.generated.resources.option_custom_headers
-import navic.composeapp.generated.resources.option_proxy_settings
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
+import paige.navic.di.LocalPlatformContext
+import paige.navic.di.PlatformType
 import paige.navic.domain.manager.PreferenceManager
 import paige.navic.domain.manager.SessionManager
 import paige.navic.icons.Icons
 import paige.navic.icons.outlined.Add
+import paige.navic.icons.outlined.Check
 import paige.navic.icons.outlined.Delete
 import paige.navic.icons.outlined.Info
 import paige.navic.ui.components.common.SegmentedListItemDefaults
@@ -65,16 +62,16 @@ import paige.navic.ui.screens.settings.components.SettingsGroupDefaults
 import paige.navic.ui.screens.settings.components.SettingsNavItem
 import paige.navic.ui.screens.settings.components.SettingsToggleItem
 import paige.navic.ui.theme.defaultFont
-import kotlin.random.Random
 
 private data class Header(
-	val id: Long = Random.nextLong(),
 	val key: String,
 	val value: String
 )
 
 @Composable
 fun SettingsConnectionOptionsScreen() {
+	val platformContext = LocalPlatformContext.current
+
 	val sessionManager = koinInject<SessionManager>()
 	val preferenceManager = koinInject<PreferenceManager>()
 
@@ -115,9 +112,12 @@ fun SettingsConnectionOptionsScreen() {
 
 	fun updateSettings() {
 		preferenceManager.customHeaders = headers
+			.filter { it.key.isNotBlank() && it.value.isNotBlank() }
 			.joinToString("\n") { "${it.key}:${it.value}" }
 		sessionManager.refreshClient()
 	}
+
+	val count = if (platformContext.platformType == PlatformType.Android) 2 else 1
 
 	Scaffold(
 		topBar = { NestedTopBar({ Text(stringResource(Res.string.option_connection_options)) }) }
@@ -153,19 +153,20 @@ fun SettingsConnectionOptionsScreen() {
 				SettingsGroup(
 					modifier = Modifier.fillMaxWidth()
 				) {
-					// TODO: hide this on iOS, and also add strings to the xml. piss
-					SettingsToggleItem(
-						checked = sslNoopChecked,
-						onCheckedChange = updateSslNoop,
-						content = { Text("Ignore SSL certificates") },
-						shapes = SegmentedListItemDefaults.segmentedShapes(
-							index = 0,
-							count = 2
-						),
-						supportingContent = {
-							Text("Only enable this if you know what you're doing. Useful if you're using a proxy for inspecting requests.")
-						}
-					)
+					if (platformContext.platformType == PlatformType.Android) {
+						SettingsToggleItem(
+							checked = sslNoopChecked,
+							onCheckedChange = updateSslNoop,
+							content = { Text("Ignore SSL certificates") },
+							shapes = SegmentedListItemDefaults.segmentedShapes(
+								index = 0,
+								count = count
+							),
+							supportingContent = {
+								Text("Only enable this if you know what you're doing. Useful if you're using a proxy for inspecting requests.")
+							}
+						)
+					}
 					SettingsNavItem(
 						content = { Text("Custom headers") },
 						supportingContent = {
@@ -173,7 +174,7 @@ fun SettingsConnectionOptionsScreen() {
 						},
 						shapes = SegmentedListItemDefaults.segmentedShapes(
 							index = 1,
-							count = 2
+							count = count
 						),
 						onClick = {
 							showHeadersBottomSheet = !showHeadersBottomSheet
@@ -212,24 +213,27 @@ private fun CustomHeadersBottomSheet(
 			verticalArrangement = Arrangement.spacedBy(16.dp)
 		) {
 			Column(
-				verticalArrangement = Arrangement.spacedBy(4.dp),
-				horizontalAlignment = Alignment.CenterHorizontally
+				verticalArrangement = Arrangement.spacedBy(8.dp)
 			) {
-				Text(
-					text = "Custom headers",
-					style = MaterialTheme.typography.headlineMediumEmphasized
-				)
-				Text(
-					text = "Additional information attached to each request",
-					style = MaterialTheme.typography.bodyMedium,
-					textAlign = TextAlign.Center
-				)
-				Spacer(
-					Modifier.padding(2.dp)
-				)
+				Column(
+					verticalArrangement = Arrangement.Center
+				) {
+					Text(
+						modifier = Modifier.padding(4.dp),
+						text = "Custom headers",
+						style = MaterialTheme.typography.headlineMediumEmphasized
+					)
+					Text(
+						text = "Additional information attached to each request",
+						style = MaterialTheme.typography.bodyMedium,
+						textAlign = TextAlign.Center
+					)
+				}
 				FilledTonalButton(
 					onClick = {
-						headers.add(Header(key = "", value = ""))
+						if (headers.none { it.key.isBlank() }) {
+							headers.add(Header(key = "", value = ""))
+						}
 						onUpdate()
 					},
 					modifier = Modifier.fillMaxWidth()
@@ -245,6 +249,8 @@ private fun CustomHeadersBottomSheet(
 			SettingsGroup(
 				modifier = Modifier.verticalScroll(rememberScrollState())
 					.weight(weight = 1f, fill = false)
+					.animateContentSize(),
+				verticalArrangement = Arrangement.spacedBy(8.dp)
 			) {
 				headers.forEachIndexed { index, header ->
 					HeaderRow(
@@ -277,6 +283,8 @@ private fun HeaderRow(
 	onSetValue: (String) -> Unit,
 	onDelete: () -> Unit
 ) {
+	var isEditing by remember { mutableStateOf(key.isBlank()) }
+
 	Surface(
 		color = MaterialTheme.colorScheme.surfaceContainer,
 		shape = MaterialTheme.shapes.large
@@ -290,37 +298,74 @@ private fun HeaderRow(
 				modifier = Modifier.weight(1f),
 				verticalArrangement = Arrangement.spacedBy(8.dp)
 			) {
-				TextField(
-					value = key,
-					onValueChange = onSetKey,
-					placeholder = { Text("Key") },
-					modifier = Modifier.fillMaxWidth(),
-					singleLine = true,
-					colors = TextFieldDefaults.colors(
-						focusedIndicatorColor = Color.Transparent,
-						unfocusedIndicatorColor = Color.Transparent
-					),
-					shape = MaterialTheme.shapes.medium
-				)
-				TextField(
-					value = value,
-					onValueChange = onSetValue,
-					placeholder = { Text("Value") },
-					modifier = Modifier.fillMaxWidth(),
-					singleLine = true,
-					colors = TextFieldDefaults.colors(
-						focusedIndicatorColor = Color.Transparent,
-						unfocusedIndicatorColor = Color.Transparent
-					),
-					shape = MaterialTheme.shapes.medium
-				)
+				if (isEditing) {
+					TextField(
+						value = key,
+						onValueChange = onSetKey,
+						placeholder = { Text("Key") },
+						modifier = Modifier.fillMaxWidth()
+							.heightIn(max = 54.dp),
+						singleLine = true,
+						colors = TextFieldDefaults.colors(
+							focusedIndicatorColor = Color.Transparent,
+							unfocusedIndicatorColor = Color.Transparent
+						),
+						shape = MaterialTheme.shapes.medium
+					)
+					TextField(
+						value = value,
+						onValueChange = onSetValue,
+						placeholder = { Text("Value") },
+						modifier = Modifier.fillMaxWidth()
+							.heightIn(max = 54.dp),
+						singleLine = true,
+						colors = TextFieldDefaults.colors(
+							focusedIndicatorColor = Color.Transparent,
+							unfocusedIndicatorColor = Color.Transparent
+						),
+						shape = MaterialTheme.shapes.medium
+					)
+				} else {
+					Text(
+						text = key
+					)
+				}
 			}
-			FilledTonalButton(
-				onClick = onDelete,
-				contentPadding = PaddingValues(horizontal = 0.dp, vertical = 16.dp),
-				shape = MaterialTheme.shapes.medium
-			) {
-				Icon(Icons.Outlined.Delete, stringResource(Res.string.action_delete))
+			Column {
+				if (isEditing) {
+					IconButton(
+						modifier = Modifier.padding(vertical = 6.dp),
+						onClick = onDelete,
+						shape = MaterialTheme.shapes.medium
+					) {
+						Icon(
+							Icons.Outlined.Delete,
+							stringResource(Res.string.action_delete)
+						)
+					}
+					IconButton(
+						modifier = Modifier.padding(vertical = 6.dp),
+						onClick = { isEditing = !isEditing },
+						shape = MaterialTheme.shapes.medium
+					) {
+						Icon(
+							Icons.Outlined.Check,
+							"Finish editing"
+						)
+					}
+				} else {
+					IconButton(
+						modifier = Modifier.padding(vertical = 6.dp),
+						onClick = { isEditing = !isEditing },
+						shape = MaterialTheme.shapes.medium
+					) {
+						// todo: this needs a pencil icon
+						Icon(
+							Icons.Outlined.Info,
+							"Finish editing"
+						)
+					}
+				}
 			}
 		}
 	}
