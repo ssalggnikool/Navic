ed8f43cebf40535d5ab1de6a5a82a967f36cadd9
Nightdavisao <nightdavisao27@protonmail.com>
2026-09-22 22:14:34 -0300

    Revert "Reapply "Add Android Auto support and fix album track ordering (#594)" (#599)"
    This reverts commit 6cfd9a18b5d4b3e0791092d9330c7943056398f6.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -176,7 +176,6 @@ kotlin {
 			implementation(libs.bundles.ktor.android)
 			implementation(libs.bundles.androidx.android)
 			implementation(libs.bundles.media3)
-			implementation(libs.kotlinx.coroutines.guava)
 		}
 
 		iosMain.dependencies {
